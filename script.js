214a4b14c7b45970793d2bd8c46b1d7b65a2a60e
janikanika <janikanika@mail.ru>
2021-09-01 13:28:25 +0300

    интеполяция

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+
+//const typeGoods = 'food';
+
+
+//console.log('https://market.com/'+ typeGoods);
+//console.log('https://market.com/'+ typeGoods + '/'+ '10');
+
+
+//console.log(`https://market.com/${typeGoods}`);
+
+
+/*
+Задание
+
+
+Вывести в консоль с помощью интеполяции  адрес
+https://market.com/games/strategy/10
+сохранить в Github. коммит - интеполяция
+
+
+
+*/
+
+"use strict";
+//console.log(`https://market.com/${games}`);
+const games = "game";
+
+const type = "type";
+
+console.log(`https://market.com/${games}/${type}`);
